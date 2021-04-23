e9f37318f0216be02dfac8ce2abe4d3226c3f84d
Jaburu <jaburu@dakimakuri.com>
2021-04-23 03:29:51 +0000

    gitpod fixes

--- start-gitpod.sh
+++ start-gitpod.sh
@@ -1,7 +1,8 @@
 URL_3000=$(gp url 3000)
 URL_1998=$(gp url 1998)
 URL_1998_WS=$(echo $URL_1998 | sed "s/https/wss/")
-sudo docker-up
+sudo docker-up&
+npx wait-on file://var/run/docker.sock
 sudo chown gitpod:gitpod /var/run/docker.sock
 echo -e "CORS_ORIGIN=$URL_3000" > packages/dvb/.env
 echo -e "NEXT_PUBLIC_GRAPHQL=$URL_1998/graphql\nNEXT_PUBLIC_GRAPHQL_WS=$URL_1998_WS/graphql" > packages/dvb-ui/.env.development.local
