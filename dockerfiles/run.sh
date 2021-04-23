95ae8eade1c791fd255353c3fa001581d1198996
Jaburu <jaburu@dakimakuri.com>
2021-04-23 04:06:54 +0000

    add a basic auth in docker

--- dockerfiles/run.sh
+++ dockerfiles/run.sh
@@ -1,3 +1,9 @@
+if [[ -v AUTH_USERNAME ]]; then
+  if [[ -v AUTH_PASSWORD ]]; then
+    printf "$AUTH_USERNAME:$(openssl passwd -apr1 $AUTH_PASSWORD)\n" > /etc/nginx/htpasswd
+    sed -i "s/#auth/auth_basic \"Login\";\n        auth_basic_user_file \/etc\/nginx\/htpasswd;/" /etc/nginx/sites-enabled/dvb
+  fi
+fi
 /etc/init.d/nginx start
 (cd packages/dvb-ui && npm start&)
 (cd packages/dvb && npm start)
