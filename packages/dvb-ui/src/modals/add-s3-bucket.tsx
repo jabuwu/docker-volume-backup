567ae762426555480695f9ef943f983eaf0f9e58
jabu <jaburu@dakimakuri.com>
2021-04-27 04:29:29 +0000

    show info on how to create IAM policy for s3

--- packages/dvb-ui/src/modals/add-s3-bucket.tsx
+++ packages/dvb-ui/src/modals/add-s3-bucket.tsx
@@ -1,19 +1,67 @@
-import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, Stack, InputGroup, InputLeftAddon, Input, ModalFooter, Button } from '@chakra-ui/react';
-import React, { useRef, useCallback } from 'react';
+import { InfoIcon } from '@chakra-ui/icons';
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, Stack, InputGroup, InputLeftAddon, Input, ModalFooter, Button, Text, Collapse, Link, Box, Code, Flex, Alert, AlertIcon } from '@chakra-ui/react';
+import React, { useRef, useCallback, useState } from 'react';
 import { AllStorageDocument, AllStorageQuery, useAddS3BucketMutation } from '../generated/graphql';
+
+function makePolicy(bucket: string, prefix: string) {
+  return {
+    Version: '2012-10-17',
+    Statement: [
+      {
+        Action: [
+          's3:DeleteObject',
+          's3:GetObject',
+          's3:ListBucket',
+          's3:PutObject'
+        ],
+        Effect: 'Allow',
+        Resource: [
+          `arn:aws:s3:::${bucket}`,
+          `arn:aws:s3:::${bucket}/${prefix}*`
+        ]
+      }
+    ]
+  };
+}
+
+function selectElementText(element) {
+  const doc = window.document as any;
+  if (window.getSelection && doc.createRange) {
+    const selection = window.getSelection();
+    const range = doc.createRange();
+    range.selectNodeContents(element);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  } else if (doc.body.createTextRange) {
+    const range = doc.body.createTextRange();
+    range.moveToElementText(element);
+    range.select();
+  }
+}
 
 export default function AddS3BucketModal({ children }: { children: (open: () => void) => JSX.Element }) {
   const [ isOpen, setIsOpen ] = React.useState(false)
   const [ addS3Bucket ] = useAddS3BucketMutation();
-  const name = useRef<any>();
-  const bucket = useRef<any>();
-  const region = useRef<any>();
-  const accessKey = useRef<any>();
-  const secretKey = useRef<any>();
-  const prefix = useRef<any>();
+  const [ showInfo, setShowInfo ] = useState(false);
+  const [ showPolicy, setShowPolicy ] = useState(false);
+  const nameRef = useRef<any>();
+  const [ name, setName ] = useState('');
+  const [ bucket, setBucket ] = useState('');
+  const [ region, setRegion ] = useState('');
+  const [ accessKey, setAccessKey ] = useState('');
+  const [ secretKey, setSecretKey ] = useState('');
+  const [ prefix, setPrefix ] = useState('');
 
   const open = useCallback(() => {
     setIsOpen(true);
+    setShowInfo(false);
+    setShowPolicy(false);
+    setName('');
+    setBucket('');
+    setRegion('');
+    setAccessKey('');
+    setSecretKey('');
+    setPrefix('');
   }, []);
 
   const close = useCallback(() => {
@@ -23,12 +71,12 @@ export default function AddS3BucketModal({ children }: { children: (open: () => 
   const add = useCallback(async () => {
     await addS3Bucket({
       variables: {
-        name: name.current!.value,
-        bucket: bucket.current!.value,
-        region: region.current!.value,
-        accessKey: accessKey.current!.value,
-        secretKey: secretKey.current!.value,
-        prefix: prefix.current!.value,
+        name,
+        bucket,
+        region,
+        accessKey,
+        secretKey,
+        prefix,
       },
       update: (cache, { data }) => {
         if (data.addS3Bucket) {
@@ -46,43 +94,67 @@ export default function AddS3BucketModal({ children }: { children: (open: () => 
   return (
     <>
       { children(open) }
-      <Modal size="xl" isOpen={ isOpen } onClose={ close }>
+      <Modal size="xl" isOpen={ isOpen } onClose={ close } initialFocusRef={ nameRef }>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Add S3 Bucket</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
+            <Text>It is highly recommended that you create a dedicated <Link href="https://aws.amazon.com/iam/" target="_blank" fontWeight="bold">IAM user</Link> with restricted permissions to your AWS account.</Text>
+            <Collapse in={ showInfo } animateOpacity>
+              <Box my={ 4 }>
+                <Text>Create a new user in your AWS account and enable programmatic access, then fill out the form below and press the <Text as="span" fontWeight="bold">Show Policy</Text> button to generate a working IAM policy that you can attach to the user. Doing so protects your AWS account in the unlikely event that your <Text as="span" fontWeight="bold">Docker Volume Backup</Text> instance becomes compromised.</Text>
+                <Text my={ 4 }></Text>
+              </Box>
+            </Collapse>
+            <Text mb={ 8 }><Link fontWeight="bold" onClick={ () => setShowInfo(value => !value) }>{ showInfo ? 'Show less' : 'Show more' }</Link></Text>
             <Stack spacing={4}>
               <InputGroup size="sm">
                 <InputLeftAddon children="Name" />
-                <Input ref={ name } />
+                <Input ref={ nameRef } value={ name } onChange={ e => setName(e.target.value) } />
               </InputGroup>
               <InputGroup size="sm">
                 <InputLeftAddon children="Bucket" />
-                <Input ref={ bucket } />
+                <Input value={ bucket } onChange={ e => setBucket(e.target.value) } />
               </InputGroup>
               <InputGroup size="sm">
                 <InputLeftAddon children="Region" />
-                <Input ref={ region } />
+                <Input value={ region } onChange={ e => setRegion(e.target.value) } />
               </InputGroup>
               <InputGroup size="sm">
                 <InputLeftAddon children="Access Key" />
-                <Input ref={ accessKey } />
+                <Input value={ accessKey } onChange={ e => setAccessKey(e.target.value) } />
               </InputGroup>
               <InputGroup size="sm">
                 <InputLeftAddon children="Secret Key" />
-                <Input ref={ secretKey } />
+                <Input value={ secretKey } onChange={ e => setSecretKey(e.target.value) } />
               </InputGroup>
               <InputGroup size="sm">
                 <InputLeftAddon children="Prefix" />
-                <Input ref={ prefix } />
+                <Input value={ prefix } onChange={ e => setPrefix(e.target.value) } />
               </InputGroup>
             </Stack>
+            <Collapse in={ showPolicy } animateOpacity>
+              { bucket ?
+                <Code mt={ 4 } as="pre" width="100%" onClick={ e => selectElementText(e.target) } cursor="pointer">{ JSON.stringify(makePolicy(bucket, prefix), null, 2) }</Code>
+              : null }
+              { !bucket ?
+                <Alert status="error" mt={ 4 }>
+                  <AlertIcon />
+                  Policy not available. Please fill out the form above.
+                </Alert>
+              : null }
+            </Collapse>
           </ModalBody>
           <ModalFooter>
-            <Button colorScheme="green" onClick={ add }>
-              Add
-            </Button>
+            <Flex width="100%">
+              <Button colorScheme="gray" onClick={ () => setShowPolicy(value => !value) } leftIcon={ <InfoIcon /> }>
+                { showPolicy ? 'Hide Policy' : 'Show Policy' }
+              </Button>
+              <Button ml="auto" colorScheme="green" onClick={ add }>
+                Add
+              </Button>
+            </Flex>
           </ModalFooter>
         </ModalContent>
       </Modal>
